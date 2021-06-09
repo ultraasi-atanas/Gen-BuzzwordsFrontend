950db6af873d67fb1607c7c07378a5f1f20c472e
CAZBUNDLE <74971704+CAZBUNDLE@users.noreply.github.com>
2021-06-09 19:52:00 +0100

    caz

--- frontend.js
+++ frontend.js
@@ -1,9 +1,28 @@
 'use strict';
 
+let bigHex = document.getElementById('bigHex')
 
 function add_img() { 
-	var img = document.createElement('img'); 
-    img.src = 'hexagon.svg' 
-	document.getElementById('body').appendChild(img);
+
+	
+	for (let i = 0; i < 52; i++) {
+		var img = document.createElement('img'); 
+		img.src = 'icons/hexagon.svg' 
+		img.className = "img2"
+		bigHex.appendChild(img);
+		
+	}
+
+	
+	
+
+	
 }
+add_img()
+
+
+
+ 
+
 console.log('asdas')
+
