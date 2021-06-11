940ba87d87e0ba7cb053c2db13d46afbdb9ee90f
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-06-11 12:39:57 +0100

    hex grid

--- frontend.js
+++ frontend.js
@@ -4,24 +4,45 @@ let bigHex = document.getElementById('bigHex')
 
 function add_img() {
 
-	let rows = 7
+	let rows = 4
+	let stop = 8
 
-	let stop = 7
+	// for (let r = 0; r <= rows; r++) {
+	// 	let row = document.createElement('div')
+	// 	bigHex.appendChild(row)
+	// 	for (let c = 0; c <= stop; c++) {
 
-	const br = "\n";
+	// 		var img = document.createElement('img');
+	// 		img.src = 'icons/hexagon.svg'
+	// 		//img.className = "img2"
+	// 		img.style.width = "30px"
+	// 		row.appendChild(img);
 
-	for (let r = 0; r <= rows; r++) {
+	// 	}
+	// }
+	for (let r = rows-1; r <= stop-1; r++) {
+
 		let row = document.createElement('div')
 		bigHex.appendChild(row)
-		for (let c = 0; c <= stop; c++) {
+
+		for (let c = r; c >= 0; c--) {
 
 			var img = document.createElement('img');
 			img.src = 'icons/hexagon.svg'
-			//img.className = "img2"
 			img.style.width = "30px"
 			row.appendChild(img);
 
 		}
 	}
+}
+add_img()
+
+let test = document.getElementById('test')
+
+for (let i = 3; i<8; i++){
+	for (let y=i; y>=0;y-- ){
+		test.append(y )
 	}
-	add_img()
+	let br = document.createElement('br');
+	test.appendChild(br)
+}
