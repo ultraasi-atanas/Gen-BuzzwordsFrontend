110c832631b2524fd9ced1fa4c5dac204988e1ca
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-06-12 12:21:56 +0100

    honeycomb

--- frontend.js
+++ frontend.js
@@ -4,45 +4,32 @@ let bigHex = document.getElementById('bigHex')
 
 function add_img() {
 
-	let rows = 4
+	let topEdge = 5
 	let stop = 8
-
-	// for (let r = 0; r <= rows; r++) {
-	// 	let row = document.createElement('div')
-	// 	bigHex.appendChild(row)
-	// 	for (let c = 0; c <= stop; c++) {
-
-	// 		var img = document.createElement('img');
-	// 		img.src = 'icons/hexagon.svg'
-	// 		//img.className = "img2"
-	// 		img.style.width = "30px"
-	// 		row.appendChild(img);
-
-	// 	}
-	// }
-	for (let r = rows; r <= stop-1; r++) {
-
-		let row = document.createElement('div')
-		bigHex.appendChild(row)
-
-		for (let c = r; c >= 0; c--) {
-
-			var img = document.createElement('img');
-			img.src = 'icons/hexagon.svg'
-			img.style.width = "30px"
-			row.appendChild(img);
-
-		}
+	
+	for (let r = topEdge; r <= stop; r++) {
+		
+		bigHex.appendChild(buildRow(r))
 	}
+	for (let r = stop-1; r >= topEdge; r--) {
+		
+		bigHex.appendChild(buildRow(r))
+	}
+
+}
+function buildRow(cols){
+	let row = document.createElement('div')
+
+	for (let c = cols; c > 0; c--) {
+
+		var img = document.createElement('img');
+		img.src = 'icons/hexagon.svg'
+		img.style.width = "30px"
+		row.appendChild(img);
+
+	}
+	return row
 }
 add_img()
 
 let test = document.getElementById('test')
-
-for (let i = 3; i<8; i++){
-	for (let y=i; y>=0;y-- ){
-		test.append(y )
-	}
-	let br = document.createElement('br');
-	test.appendChild(br)
-}
