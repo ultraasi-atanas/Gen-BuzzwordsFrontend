3a8f3ac867905a26f7b1ef10c0b1eb1041984940
stephen-hooper-git <stephen.hooper@outlook.com>
2021-06-19 10:26:44 +0100

    Fixed allowed cells when submitting new word

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let allowedCells = ['c0','c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'];
+let allowedCells = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'];
 
 let usedCells = [];
 
@@ -12,14 +12,14 @@ const currentWord = document.getElementById('word');
 
 const cells = document.querySelectorAll('.cell');
 //alert(cells.length);
-cells.forEach(c => {c.addEventListener('click', () => cellClick(c))})
+cells.forEach(c => { c.addEventListener('click', () => cellClick(c)) })
 
 fillBoard(10)
-async function fillBoard (numLetters){
+async function fillBoard(numLetters) {
 
     let board = await submit('GET', `http://localhost:3000/api/rndletters/${numLetters}`)
 
-    for (let i=0; i<board.length; i++) {
+    for (let i = 0; i < board.length; i++) {
 
         let id = 'c' + i
         let tile = document.getElementById(id)
@@ -43,9 +43,8 @@ const cellClick = cell => {
         // Change background color of the clicked cell
         //cell.style.backgroundColor = 'peru';
         cell.classList.add("used")
-       
         word = word + cell.innerHTML
-        letters[cell.id] =cell.innerHTML
+        letters[cell.id] = cell.innerHTML
         selectedTiles.push(cell)
         currentWord.innerHTML = word
     } else {
@@ -53,7 +52,7 @@ const cellClick = cell => {
     }
 }
 
-function clearCell(){
+function clearCell() {
     cells.forEach(c => {
         c.classList.remove("used")
         c.classList.remove("good")
@@ -64,74 +63,78 @@ function clearCell(){
 }
 
 async function submitWord() {
-   let response = await submit('POST', 'http://localhost:3000/api/dict' ,{"letters":letters})
-   console.log(response)
-   const history=document.getElementById("wordHistory")
-   let words=document.createElement("p") 
-   words.innerHTML=`${response.word} ${response.score}`
-   history.appendChild(words)
+    let response = await submit('POST', 'http://localhost:3000/api/dict', { "letters": letters })
+    console.log(response)
+    const history = document.getElementById("wordHistory")
+    let words = document.createElement("p")
+    words.innerHTML = `${response.word} ${response.score}`
+    history.appendChild(words)
 
-   //const cells = document.querySelectorAll('.cell');
-  //alert(cells.length);
+    //const cells = document.querySelectorAll('.cell');
+    //alert(cells.length);
 
-  clearCell()
+    clearCell()
 
-   if (response.match){
-    selectedTiles.forEach(t=>{
-        t.classList.add("good")
+    if (response.match) {
+        selectedTiles.forEach(t => {
+            t.classList.add("good")
         })
     }
-   else {
-    selectedTiles.forEach(t=>{
-        t.classList.add("bad")
+    else {
+        selectedTiles.forEach(t => {
+            t.classList.add("bad")
         })
     }
-    letters={}
-    selectedTiles=[]
-    usedCells =[]
+
+    letters = {}
+    selectedTiles = []
+    allowedCells = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'];
+    usedCells = []
+    word = '';
     currentWord.innerHTML = ""
-   // we receive new letters as part of the response: refill the used letters
-   console.log(response.letters)
-   for(let k in response.letters){
-       let tile =document.getElementById(k) 
-       tile.innerHTML = response.letters[k]
-   }
-   //(textcontent appendChild)
-   
-   // TODO - multiple history items   
-   // Flash/remove/animate correct letters
-   // Show letters as 'bad' if there was no match (penalty??)
-   // Use/show the replacement letters      
-   // Show running total score (will need to come from server)
+
+    // we receive new letters as part of the response: refill the used letters
+    console.log(response.letters)
+    for (let k in response.letters) {
+        let tile = document.getElementById(k)
+        tile.innerHTML = response.letters[k]
+    }
+    //(textcontent appendChild)
+
+    // TODO - multiple history items   
+    // Flash/remove/animate correct letters
+    // Show letters as 'bad' if there was no match (penalty??)
+    // Use/show the replacement letters      
+    // Show running total score (will need to come from server)
 
 }
 
-async function submit(method, url,requestBodyObj){
-    
-    
-    let payload=null
+async function submit(method, url, requestBodyObj) {
 
-    if (method=="POST"){
+
+    let payload = null
+
+    if (method == "POST") {
         payload = JSON.stringify(requestBodyObj) // the trick here is to make an object from the formdata
-        console.log ("PL:" + payload)
+        console.log("PL:" + payload)
     }
-      
-    
-    const response = await fetch(url, {method:method,body:payload,headers:{'Accept':'application/json','Content-Type':'application/json'}})
+
+
+    const response = await fetch(url, { method: method, body: payload, headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } })
     //const response = await fetch(url, {method:method,headers:{'Accept':'application/json','Content-Type':'application/json'}})
-  
-    if (response.ok){     
-        const promise = await response.json() 
+
+    if (response.ok) {
+        const promise = await response.json()
         //console.log(obj[0].word + obj[0].meanings[0].definitions[0].definition)       
         return (promise)
 
-	// Do something with the object we just receved
-    
+        // Do something with the object we just receved
+
     }
-    else{
-	//something bad happened
-	document.getElementById("message").innerHTML= + response.status
-        setTimeout(()=>{document.getElementById("message").innerHTML= ""},1000)
+    else {
+        //something bad happened
+        document.getElementById("message").innerHTML = + response.status
+        setTimeout(() => { document.getElementById("message").innerHTML = "" }, 1000)
 
     }
 }
