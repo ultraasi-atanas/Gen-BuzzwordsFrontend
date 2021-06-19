e9dc55a66f2253f286e2f89f7c7157896e916f27
CAZBUNDLE <74971704+CAZBUNDLE@users.noreply.github.com>
2021-06-19 14:54:57 +0100

    caz update

--- frontend.js
+++ frontend.js
@@ -2,45 +2,51 @@
 
 let bigHex = document.getElementById('bigHex')
 
-function add_img() {
+function makeGrid() {
 	let topEdge = 3
 	let stop = 6
-	for (let r = topEdge; r <= stop; r++) {
+	let startNum = 0
+	for (let cols = topEdge; cols <= stop; cols++) {
 
-		bigHex.appendChild(buildRow(r))
-
+		bigHex.appendChild(buildRow(cols,startNum))       
+		startNum += cols
 	}
-	for (let r = stop - 1; r >= topEdge; r--) {
+	for (let cols = stop - 1; cols >= topEdge; cols--) {
 
-		bigHex.appendChild(buildRow(r))
+		bigHex.appendChild(buildRow(cols,startNum))
+		startNum += cols
 	}
 
 }
 
-function buildRow(cols) {
+function buildRow(cols,startNum) {
 	let row = document.createElement('div')
 
-	for (let c = cols; c > 0; c--) {
-
+	// for (let c = cols; c > 0; c--) 
+	for (let c = 0; c < cols; c++) {
 		var img = document.createElement('img');
 		img.src = 'icons/hexagon.svg'
-		img.style.width = "30px"
+		img.style.width = "30px" 
+		let id = c+startNum
+		img.setAttribute("id", id)
+		img.setAttribute("data-left",id -1)  // wil be ID number ...do we get rid of code that makes it
+
 		row.appendChild(img);
 
 
 	}
 	return row
 }
-add_img()
+makeGrid()
 
 let images = document.getElementsByTagName("img")[0];
 
-function attributes() {
-	for (let i = 0; i <= images; i++) {
-		images.setAttribute("id", counter++)
+// function attributes() {
+// 	for (let i = 0; i <= images; i++) {
+		
 
-	}
-}
+// 	}
+// }
 
 attributes()
 
@@ -48,21 +54,23 @@ let counter = 0
 let test = document.getElementById('test')
 
 
-// Case 4 =
-// 	TL = n - c
-// TR = (n - c) + 1
-// BL = n + c
-// BR = BL + 1
+let imgs = document.getElementsByTagName('img');  //To the look will give them each an IDThis is to give each hex an ID using IMG //seperate funtion.This will always give any element an ID regardless of its use.
+for (let i = 0, length = imgs.length; i < length; i++) {
+    imgs[i].setAttribute("id", i);  
+}
 
-// Case 20 =
-// 	TL = n - (c + 1)
-// TR = n - c
-// BL = n + (c - 1)
-// BR = n + c
+// let tl 
+// let tr
+// let r
+// let l
+// let bl
+// let br
+// let n = id
+// let c 	
+// let col = colum loop  counter ++
+// let hex 
 
-
-
-
+// function neighbours (id,c){
 
 
 
